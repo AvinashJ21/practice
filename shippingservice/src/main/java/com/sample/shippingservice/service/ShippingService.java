a7a75ffaa286b9a5582349dcca72150d54e0858b
AvinashJ21 <avinashjayakar@gmail.com>
2024-06-09 23:41:21 +0530

    updated

--- shippingservice/src/main/java/com/sample/shippingservice/service/ShippingService.java
+++ shippingservice/src/main/java/com/sample/shippingservice/service/ShippingService.java
@@ -2,6 +2,9 @@ package com.sample.shippingservice.service;
 
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,8 @@ public class ShippingService {
 	@Autowired
 	ShippingRepository shippingRepo;
 	
+	Logger log = LoggerFactory.getLogger(ShippingService.class);
+	
 	 public Long createShippingOrder() {
 		 
 		 	OrderDetailsJaxbDto orderDto = JaxbXMLToObject.xmltoObject();
@@ -25,7 +30,9 @@ public class ShippingService {
 		 		 BeanUtils.copyProperties(orderDto, se);
 		 		 se.setShippingTransactionId(UUID.randomUUID().toString());
 		 	   	 se = shippingRepo.save(se);
+		 	   	 MDC.put("ShippingTransaction-Id", se.getShippingTransactionId());
 		 	}
+		 	log.debug("Shipping Order Created");
 		 	return se.getShippingId();
 		 	
 	 }
